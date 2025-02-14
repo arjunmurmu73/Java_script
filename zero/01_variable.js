b08caf4a16117d17b3f51dbe58c12f2ee5a87292
arjunmurmu73 <arjun@vssut.ac.in>
2025-02-14 23:47:39 +0530

    Create 01_variable.js

---
+++ zero/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 144553 //const meaning the constant , no change
+let Email_id = "arjun@gmail.com"
+var accountPassword = "12345"
+account_city = "Balasore"
+// console.log(Email_id);
+// console.log(accountId);
+
+//accountId = 23456 // not allowed beacuse use const
+
+accountPassword = "arjun"
+Email_id = "murmu@gmail.com"
+account_city = "sambalpur"
+let my_address;
+console.log([accountId , Email_id, accountPassword, account_city,my_address]);  
+account_name = "Arjun Mumru"
+console.log(account_name);
+
+
+
