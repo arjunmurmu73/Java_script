f0bc4282cb4be3382960a288e3077b894d741aa5
arjunmurmu73 <arjun@vssut.ac.in>
2025-02-15 04:21:30 +0000

    data_type

---
+++ zero/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict "; //treate all JS code as newer version
+
+//alert(3+3) //we are using nodeJs, not browser
+console.log(9+9);
+
+//Always try to code is readybalility should be high
+
+//Documentery : mdn and 
+// tc39: https://tc39.es/ecma262/#sec-ecmascript-data-types-and-values
+console.log("Arjun Murmu");
+
+let name = "Arjun" // string datatype
+let age = 22 //integer datatype
+
+let isLoggedIn = false //boolen datatype
+
+
+//number => 2 to power 53
+// bigint
+//string  => " "
+//boolean => true/false
+//null => standlone value (empty value) ex: let state = null
+//undefined =>  ex: let state;
+//symbol => unique 
+
+//object 
+
+console.log(typeof "arjun")
+console.log(typeof age);
+console.log((typeof null)); //object type
+console.log(typeof undefined); //self type undefined
+
+
+
