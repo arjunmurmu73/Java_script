d8baf42c049caacd2d2261ac654ba4a39055ae89
Arjun Murmu <arjun@vssut.ac.in>
2025-02-16 03:09:54 +0000

    memory

--- zero/datatype_summary.js
+++ zero/datatype_summary.js
@@ -38,7 +38,7 @@ let myObj = {
     name : "Arjun",
     age : 21
 }
-console.log(myObj);
+//console.log(myObj);
 
 //Function
 const myFunction = function(){
@@ -49,10 +49,35 @@ const myFunction = function(){
 // myFunction();
 // console.log(myFunction()); 
 
-console.log(("Types of data type : "),["Score : ",typeof score],["Score Value : ",typeof scoreValue],["isLoggedIn : ",typeof isLoggedIn],
-    ["Outside Temp: ",typeof outsideTemp],["ID: ",typeof Id],["anotherId: ",typeof anotherId],["Big Number: ",typeof bigNumber],
-    ["Heros : ",typeof heros],["myObj: ",myObj],["Myfunction: ",typeof myFunction])
+// console.log(("Types of data type : "),["Score : ",typeof score],["Score Value : ",typeof scoreValue],["isLoggedIn : ",typeof isLoggedIn],
+//     ["Outside Temp: ",typeof outsideTemp],["ID: ",typeof Id],["anotherId: ",typeof anotherId],["Big Number: ",typeof bigNumber],
+//     ["Heros : ",typeof heros],["myObj: ",myObj],["Myfunction: ",typeof myFunction])
 
 
-    
+
 // Link of study The type of Operator : https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//****************************Memory**************************************
+//Two Type of memory : stack, heap
+//1. stack : (primitive) -> variable copy value
+//2. heap : (non primitve) -> reference and original value
+
+//Example: 
+let myytname = "a murmu creation."
+let myytname2 = myytname
+// console.log(myytname2);
+myytname2 = "jaga bhai vlog."
+// console.log("Myytname : ",myytname);
+// console.log("Myytname2 : ",myytname2);
+
+let user1 = {
+    name : "arjun murmu",
+    upi : "arjun@upi.in"
+}
+console.log("userone : ",user1);
+let user2 = user1;
+console.log("user two : ",user2);
+user2.name = "Jaga murmu";
+console.log("userone : ",user1);
+console.log("user two : ",user2);
