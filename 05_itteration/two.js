ace58aa72f29c72b04e3603ff473e7f1a1c3acbf
Arjun Murmu <arjun@vssut.ac.in>
2025-04-02 16:00:39 +0530

    Update two.js

--- 05_itteration/two.js
+++ 05_itteration/two.js
@@ -26,6 +26,8 @@ while(arr < myStudents.length){
 
 let score = 11
 do{
-    console.log(`Score is : ${score}`);
+    //console.log(`Score is : ${score}`);
     score++;
 } while ( score <= 10);
+
+
